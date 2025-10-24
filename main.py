a0865ac3a3323015ac651d46a702fb9ac30e7d8c
Michael Welte <mikewelte22@gmail.com>
2025-10-24 09:25:19 -0400

    Adding health endpoint

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from routes.vector_search import router
+from routes.vector_search import router as vector_router
+from routes.health import router as health_router
 
 app = FastAPI(title="FastAPI Integration Test Example")
-app.include_router(router=router)
+app.include_router(router=vector_router)
+app.include_router(router=health_router)
